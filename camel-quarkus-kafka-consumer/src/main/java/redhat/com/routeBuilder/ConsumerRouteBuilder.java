a15c215423df90a5b78151314a47546c40a8e306
EricSaDev <eric.almeida.sa@gmail.com>
2023-05-24 15:45:19 -0300

    Fixing randon file name

--- camel-quarkus-kafka-consumer/src/main/java/redhat/com/routeBuilder/ConsumerRouteBuilder.java
+++ camel-quarkus-kafka-consumer/src/main/java/redhat/com/routeBuilder/ConsumerRouteBuilder.java
@@ -17,7 +17,8 @@ public class ConsumerRouteBuilder extends RouteBuilder{
         from("kafka:"+ KAFKA_TOPIC + "?brokers=" + KAFKA_BOOTSTRAP_SERVERS + "&groupId=" + KAFKA_GROUP_ID)
         .routeId("kafkaConsumer")
         .log("Message received from Kafka : ${body}")
-        .to("file:/tmp/?fileName=message-" + new SimpleDateFormat("yyyy-MM-dd-HH-mm:ss.SSS", Locale.US).format(new Date()) + ".txt")
+        .setHeader("CamelFileName", constant("message-" + new SimpleDateFormat("yyyy-MM-dd-HH-mm:ss.SSS", Locale.US).format(new Date()) + ".txt"))
+        .to( "file:/tmp");
         ;
     }
 }
